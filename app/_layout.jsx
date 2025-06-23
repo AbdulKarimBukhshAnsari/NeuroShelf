1e00a2a83f939d63617fce4a737cae15a405b112
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2025-06-23 23:58:06 +0500

    feat : Added the fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,33 @@
-import { Stack } from 'expo-router';
+import { Stack , SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import "../global.css"
+import { useEffect } from 'react';
 
 export default function Layout() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsloaded , error] = useFonts({
+    "Poppins-Bold" : require('../assets/fonts/Poppins-Bold.ttf') , 
+    "Poppins-Regular" : require('../assets/fonts/Poppins-Regular.ttf'),
+    "Poppins-Medium" : require('../assets/fonts/Poppins-Medium.ttf')
+  }) 
+
+  useEffect(() => {
+    if(error) throw error 
+    if(fontsloaded) SplashScreen.hideAsync();
+  }, [fontsloaded , error])
+
+  if(!fontsloaded && !error) {
+    return null ;
+  }
+  
+
+
+
   return (
     <SafeAreaProvider>
-      <Stack />
+      <Stack screenOptions={{headerShown :false}}/>
     </SafeAreaProvider>
   );
 }
